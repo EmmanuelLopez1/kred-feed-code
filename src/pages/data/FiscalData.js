f42d45368773a3b79f5b2b8cfef0bfcd54fc4de4
maxo <maximino33@hotmail.com>
2021-10-12 01:34:21 -0500

    Page Informacion Fiscal Optimizada

---
+++ src/pages/data/FiscalData.js
@@ -0,0 +1,21 @@
+
+
+export const FiscalData = [
+        {
+            placeholder: 'Razon social',
+            text:'Es el nombre oficial por el que la empresa consta legalmente, es decir, la denominación que aparece en los documentos oficiales de constitución de la compañía como persona jurídica, y se inscribe en el Registro Público de la Propiedad y el Comercio.',
+            link :'https://www.conceptosjuridicos.com/mx/razon-social/'
+        },
+        {
+            placeholder: 'Nombre comercial',
+            text:'El nombre comercial es aquel que sirve para que los consumidores y el público general conozca a una empresa, mientras que la razón social se utiliza para identificar de forma legal y oficial a una empresa.',
+            link :'https://www.conceptosjuridicos.com/mx/razon-social/'
+        },
+        {
+            placeholder: 'Regimen Fiscal',
+            text:'Un régimen fiscal es el conjunto de leyes y normas que rigen la situación tributaria de personas físicas y personas morales al momento de registrarse en el SAT (Servicio de Administración Tributaria).',
+            link :'https://www.sat.gob.mx/consulta/09788/emprendedor-conoce-los-regimenes-fiscales'
+        },
+
+    ]
+
